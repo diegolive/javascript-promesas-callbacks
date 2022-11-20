d4520342ec04f1ad4c065db2f7949eac88c2b290
diegolive <diegolive@gmail.com>
2022-11-20 16:26:52 +0100

    Hasta callbacks hell

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import { buscarHeroe } from './js/callbacks'
 import './styles.css';
 
-console.log('Hola mundo!!!');
+const heroeId1 = 'capi';
+const heroeId2 = 'iron2';
+
+buscarHeroe( heroeId1, (err,heroe1) => {
+    if (err) { return console.error(err); }
+
+    buscarHeroe( heroeId2, (err,heroe2) => {
+        if (err) { return console.error(err); }
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
+        // Y si tengo qu enviar más héroes tengo que anidar mucho más eso es el "Callback Hell"
+    });
+
+
+});
+
+console.log('Fin del programa.');
